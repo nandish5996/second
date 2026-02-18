3d23b56e41a00430fbacd9fa8e59c4c1ce18cd4f
nandish5996 <nandishnaik321@gmail.com>
2026-02-18 15:46:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import New from './component/new'
+
+function App() {
+
+  return ( 
+    <>
+      <div className="app">
+       
+      </div>
+      
+    
+    <New/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/new.jsx
@@ -0,0 +1,38 @@
+import './new.css'
+function New() {
+
+
+  return (
+    <>
+    <div className="footer">
+        <div className='foot'>
+          <div className='footl'>
+          <div className='foot1'>
+            <div className='foot1t'>
+              <div className='tlog'></div>
+              <div className='tlogname'>StarWish</div>
+            </div>
+            <div className='foot1b'></div>
+          </div>
+          <div className='foot2'>
+            <div className='foot2t'>Quick Links</div>
+            <div className='foot2b'></div>
+          </div>
+          </div>
+          <div className='footr'>
+          <div className='foot3'>
+            <div className='foot3t'>For Celebrities</div>
+            <div className='foot3b'></div>
+          </div>
+          <div className='foot4'>
+             <div className='foot4t'>Company</div>
+            <div className='foot4b'></div>
+          </div>
+          </div>
+        </div>
+        <div className='footb'> ©2024 Starwish. All rights reserved</div>
+    </div>
+    </>
+     )
+}
+export default New
